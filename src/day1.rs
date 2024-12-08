755073961cb13069281392c10e5e28888b2b3c3c
Graham Strickland <grahamhstrickland@gmail.com>
2024-12-08 14:19:18 +0200

    Add read function for tsv file

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,13 @@
+use std::io::{self, prelude::*, BufReader};
+
+pub fn read(content: &[u8]) -> io::Result<(Vec<i32>, Vec<i32>)> {
+    return Ok(BufReader::new(content).lines().map(|line| {
+        let line = line.unwrap();
+        let mut pair = line.split("\t").map(|s|s.parse::<i32>().unwrap());
+        (pair.next().unwrap(), pair.next().unwrap())
+    }).collect::<(Vec<i32>, Vec<i32>)>())
+}
+
 pub fn list_diff(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
     let mut diff = 0;
     let mut new_list1 = list1.clone();
@@ -7,7 +17,7 @@ pub fn list_diff(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
     new_list2.sort();
 
     for (item1, item2) in new_list1.iter().zip(new_list2.iter()) {
-        diff += (item1 - item2).abs()
+        diff += (item2 - item1).abs()
     }
 
     diff
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
 mod day1;
 
 pub use crate::day1::list_diff;
+pub use crate::day1::read;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_read() {
+        let content = "3\t4\n4\t3\n2\t5\n1\t3\n3\t9\n3\t3\n";
+        let res = read(content.as_bytes());
+        assert!(res.is_ok(), "Failed to read contents");
+        assert_eq!(res.ok(), Some((vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3])));
+    }
 
     #[test]
     fn test_list_diff() {
